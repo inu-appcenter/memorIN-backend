f4eab1eb00d310f03063995e6a341bf0d4afddd8
gunhee05090-bit <gunhee05090@gmail.com>
2026-07-08 22:25:21 +0900

    Simplify Visibility_type enum definition
    Refactor Visibility_type enum to remove constructor and field.

--- backend/src/main/java/com/memorin/domain/posts/Entity/Visibility_type.java
+++ backend/src/main/java/com/memorin/domain/posts/Entity/Visibility_type.java
@@ -1,18 +1,9 @@
 package com.memorin.domain.posts.Entity;
 
 public enum Visibility_type {
-
-    PUBLIC("전체 공개"),
-    FRIENDS("친구에게만 공개"),
-    PRIVATE("비공개");
-
-    private String visibility_type;
     
-    Visibility_type(String visibility_type) {
-        this.visibility_type = visibility_type;
-    }
-
-     public String getvisibility_type() {
-        return visibility_type;
-    }
+    PUBLIC,
+    FRIENDS,
+    PRIVATE;
+    
 }
